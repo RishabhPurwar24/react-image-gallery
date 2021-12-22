fd67cc484cdca6e48a5dee758bea60f5ae9d01ed
RishabhPurwar24 <rishabh15772@gmail.com>
2021-12-22 23:59:48 +0530

    update import for useState

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useState } from "react";
 
 const Search = (props) => {
   const [text, setText] = useState("");
